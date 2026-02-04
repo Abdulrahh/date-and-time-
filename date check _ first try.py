82d79b8e21599a19deb1f2dd80f1bdbcde439efc
AbdulRahman  Surakat <abdulrahmansurakat@gmail.com>
2026-02-04 13:05:26 +0000

    Update date check _ first try.py

--- date check _ first try.py
+++ date check _ first try.py
@@ -1,6 +1,7 @@
 # code to check if the current date and time has passed a certain date and time 
 # p.s this file runs an infinite loop proceed with caution 
 import datetime
+import time 
 
 target_datetime = datetime.datetime(9999, 2, 4, 12, 14, 0) 
 
